02273120fadcb10baa16eca0f3e7337e88143448
RaevNur <tanbaynurbol@gmail.com>
2018-11-25 23:19:24 +0600

    'обновление'

--- botWebhook.py
+++ botWebhook.py
@@ -1,29 +1,34 @@
-import os, requests, telebot
-from flask import Flask
+import telebot, os, logging
+from flask import Flask, request
 
-TOKEN = '739393314:AAHWAmhb3lYsP-j2xhlejFMbojqXhRZigMo'
+TOKEN = "739393314:AAHWAmhb3lYsP-j2xhlejFMbojqXhRZigMo"
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(func = lambda message: True, content_types = ['text'])
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-@server.route('/' + TOKEN, methods = ['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
-    return '!', 200
 
-@server.route('/')
-def webhook():
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+		
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://guarded-mesa-96076.herokuapp.com/bot")
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
     bot.remove_webhook()
-    bot.set_webhook(url='https://guarded-mesa-96076.herokuapp.com/' + TOKEN)
-    return '!', 200
-
-
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', '8443')))
+    bot.polling(none_stop=True)
